811bbeea12b28d39073a706a1495504a2c876ee2
anastasia9000 <anastasia.slaughte32@gmail.com>
2020-08-06 19:59:58 -0500

    Added index.js and index.html

---
+++ index.js
@@ -0,0 +1,14 @@
+let age = 16;
+console.log (age);
+let favoritecolor = "Purple"
+console.log(favoritecolor);
+ let color = `Anastasia's favorite color is ${favoritecolor}`;
+ console.log(color);
+ let horrormovie = "jigsaw"
+ console.log(horrormovie)
+ let hero = "Captain America or Iron man";
+ let favoritehero = `my favoreite superheros are ${hero}`;
+ console.log(favoritehero)
+ let height = false ;
+ console.log(height)
+ 
